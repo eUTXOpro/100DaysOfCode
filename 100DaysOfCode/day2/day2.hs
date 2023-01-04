321afb2b5e4ad2f60f5c2daa4bbd059018637eae
eutxopro <info@eutxo.pro>
2023-01-04 18:19:56 +0000

    vm backup for day 2

---
+++ 100DaysOfCode/day2/day2.hs
@@ -0,0 +1,27 @@
+-- Day 2
+
+{- 
+Write a function 'isPalindrome' that takes a list of elements
+and returns 'true' if the list is a palindrome' 
+
+TO DO / Questions For Today: 
+What is a function and how do I make one? 
+
+
+
+How do I turn a string into a list? 
+-}
+
+
+import Data.List.Split
+
+main :: IO ()
+main = do
+
+putStrLn "Enter a word, this program will check it it is a palindrome!"
+
+
+
+
+if c == b then putStrLn "It's a palindrome!"
+    else putStrLn "Yeah, it's not a palindrome"
