d4544a705666fe5c0656c955e5996ee6f7672ab9
eutxopro <info@eutxo.pro>
2023-01-04 17:58:34 +0000

    Uploading stuff from day 1

---
+++ 100DaysOfCode/day1/HaskellTestTest.hs
@@ -0,0 +1,14 @@
+-- Thanks ChatGPT! (and the_lazer_aio)
+main :: IO ()
+main = do
+
+-- defining variables
+let a = "test"
+
+-- ChatGPT test
+let b = "hannah"
+let c = "horse"
+let d = "dogeeseseegod"
+
+if a == b then putStrLn "It's a palindrome!"
+    else putStrLn "Yeah, it's not a palindrome"
