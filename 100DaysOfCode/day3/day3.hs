2ee3750debc8e23a1d8dda3047ba7e6bc334d41f
eutxopro <info@eutxo.pro>
2023-01-05 20:33:05 +0000

    Task Complete!

--- 100DaysOfCode/day3/day3.hs
+++ 100DaysOfCode/day3/day3.hs
@@ -4,17 +4,31 @@
 Write a function 'isPalindrome' that takes a list of elements
 and returns 'true' if the list is a palindrome' -}
 
-import Data.List.Split
+import Data.List
 
 -- Can't figure out how to import package using ChatGPT yet.
+-- Problem with package manager??? Data.List.Split not recongized
+-- Made the executive decicion to use a different module (Data.List)
+
+flipString :: String -> String
+flipString s = reverse s
 
 main :: IO ()
 main = do
 
 putStrLn "Enter a word, this program will check it it is a palindrome!"
-checkMe <- getLine
-let checkMeAsList = tolist checkMe
+s <- getLine
 
-putStrLn ("output = " ++ checkMe)
+let checked = flipString s
 
---This still doesn't work, but that's because there's nothing of value here yet
+if s == checked then putStrLn "It's a palindrome!"
+    else putStrLn "Yeah, it's not a palindrome"
+
+{- SUCCESS! I was able to complete the first challenge from ChatGPT (as described above)
+
+LEARNING OUTCOMES:
+- How to write an if statement
+- How to create a variable
+- How to get user imput from the terminal
+- How to define a function}
+
