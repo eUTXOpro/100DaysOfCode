e807963ba72d327290ed6cce5f83a4a56b1544ce
eutxopro <info@eutxo.pro>
2023-01-07 19:21:00 +0000

    Function

---
+++ 100DaysOfCode/day4/day4.hs
@@ -0,0 +1,3 @@
+-- Day 4
+
+{- Fixed enviroment (I fucked it up and had to reinstall linux VM)
---
+++ 100DaysOfCode/day5/day5.hs
@@ -0,0 +1,28 @@
+-- Day 5
+
+{- CURRENT OBJECTIVE
+
+Write a function that takes a list of integers and returns their sum. 
+For example, the call sum [1, 2, 3] should return 6.
+
+-}
+
+-- Define the 'sumNumbers' function, which takes a list of strings (farts) and returns an IO action*
+sumNumbers farts = do 
+    -- Apply the lambda function (\msg -> putStrLn msg >> getLine) to each element of the 'farts' list using 'mapM'
+    xs <- mapM (\msg -> putStrLn msg >> getLine) farts
+    -- Parse the strings in the 'xs' list as integers
+    let xyz = map read xs :: [Int]
+    -- Return the sum of the integers
+    return $ sum xyz
+
+-- Define the 'main' function, which is the entry point of the program
+main :: IO ()
+main = do
+    -- Call the 'sumNumbers' function with the list of strings ["Enter a number:", "Enter another number:", "Enter a third number:"]
+    -- and bind the result to the 'result' variable
+    result <- sumNumbers ["Enter a number:", "Enter another number:", "Enter a third number:"]
+    -- Print the value of 'result'
+    print result
+
+
