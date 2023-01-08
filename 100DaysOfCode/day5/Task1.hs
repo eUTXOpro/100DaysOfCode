fad39bb505350ad7f11c1f75b222c0c1a4e0ac11
EveryEpoch <100806207+EveryEpoch@users.noreply.github.com>
2023-01-08 20:48:57 +0000

    testing on wsl

--- 100DaysOfCode/day5/Task1.hs
+++ 100DaysOfCode/day5/Task1.hs
@@ -9,7 +9,8 @@ For example, the call sum [1, 2, 3] should return 6.
 
 -- Define the 'sumNumbers' function, which takes a list of strings (farts) and returns an IO action*
 sumNumbers farts = do 
-    -- Apply the lambda function (\msg -> putStrLn msg >> getLine) to each element of the 'farts' list using 'mapM'
+    -- Apply the lambda function (\msg -> putStrLn msg >> getLine) to each element of the 'farts'
+    -- list using 'mapM'
     xs <- mapM (\msg -> putStrLn msg >> getLine) farts
     -- Parse the strings in the 'xs' list as integers
     let xyz = map read xs :: [Int]
@@ -19,7 +20,8 @@ sumNumbers farts = do
 -- Define the 'main' function, which is the entry point of the program
 main :: IO ()
 main = do
-    -- Call the 'sumNumbers' function with the list of strings ["Enter a number:", "Enter another number:", "Enter a third number:"]
+    -- Call the 'sumNumbers' function with the list of strings 
+    -- ["Enter a number:", "Enter another number:", "Enter a third number:"]
     -- and bind the result to the 'result' variable
     result <- sumNumbers ["Enter a number:", "Enter another number:", "Enter a third number:", "Do it again"]
     -- Print the value of 'result'
