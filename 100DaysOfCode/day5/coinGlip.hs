ed380c65e5827ccde99c0d402486f9c2970fc0e1
Your Name~clear <info@eutxo.pro>
2023-01-08 04:00:47 +0000

    getting enviroment ready on alt OS - small changes

--- 100DaysOfCode/day5/coinGlip.hs
+++ 100DaysOfCode/day5/coinGlip.hs
@@ -1,20 +1,14 @@
 -- Day 5
-
 import System.Random
 
--- Define the 'randomInt' function, which returns an IO action that generates a random integer between 0 and 1 (inclusive)
 randomInt :: IO Int
 randomInt = do 
    n <- getStdGen  -- Get the global random number generator
    return $ fst $ randomR (0, 1) n
 
-
 main :: IO ()
 main = do
-
-n <- randomInt
-
--- Heads = Kali
--- Tails = Parrot
-
-if n == 1 then putStrLn "Heads" else putStrLn "Tails"
+  n <- randomInt
+  -- Heads = Kali
+  -- Tails = Parrot
+  if n == 1 then putStrLn "Heads" else putStrLn "Tails"
