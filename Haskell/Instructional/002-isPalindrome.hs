54a83e3b7b5b4cf88bef3a1882b39a6194fd54de
eutxopro <info@eutxo.pro>
2023-01-19 16:14:48 +0000

    improving comments

--- Haskell/Instructional/002-isPalindrome.hs
+++ Haskell/Instructional/002-isPalindrome.hs
@@ -1,10 +1,23 @@
-flipString :: String -> String {-
-fliptstring :: String -> String
+flipString :: String -> String              {- flipString :: String -> String   
+    flipstring
+     This is a function that is defined prior to the "main" function.
+     It can be called in the "main" function as "flipString"
+     Functions must start with a lower-case character. 
+     It is good practice to use camelCase when naming a function.
+    ::
+     This is used to specify the type or value of a function. 
+     It can be read as "has the type of"
+     When you define a function, you must define its type.
+     Gemerally functions must return a value however there are some exceptions.
+    String -> String
+     This is the type setting of the function.
+     Haskell is a statically-typed language and must know the type of a value at compile time.
+     This function takes a String and produces a String. -}
+flipString s = reverse s                    {- flipString s = reverse s
 
+    flipString s =
+     This defines the -}
 
--}
-
-flipString s = reverse s
 
 main :: IO ()
 main = do
